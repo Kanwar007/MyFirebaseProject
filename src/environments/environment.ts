1d32efca15a216456e5cb9ae8becda120b5e49b0
Kanwar007 <vijayblp.kanwar@gmail.com>
2020-04-24 17:09:16 +0800

    --- Auto-Git Commit ---

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firbases:{
+    apiKey: "AIzaSyBE2OcVgRkOeN-yMQs19CBpkDGP8yKgkko",
+    authDomain: "myfirebaseproject-1ad6c.firebaseapp.com",
+    databaseURL: "https://myfirebaseproject-1ad6c.firebaseio.com",
+  projectId: "myfirebaseproject-1ad6c",
+  storageBucket: "myfirebaseproject-1ad6c.appspot.com",
+  messagingSenderId: "440031605466",
+  appId: "1:440031605466:web:78a46aefc8ccdd4372abe3",
+  measurementId: "G-SWGZM25J2P"
+  }
+          
 };
 
 /*
