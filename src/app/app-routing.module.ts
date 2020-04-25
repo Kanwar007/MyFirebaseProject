112d403b228b18a34bbe929b696595c5b633cb02
Kanwar007 <vijayblp.kanwar@gmail.com>
2020-04-26 01:39:40 +0800

    --- Auto-Git Commit ---

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListOfStudentComponent } from './list-of-student/list-of-student.component';
+import { AddSudentInfoComponent } from './add-sudent-info/add-sudent-info.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListOfStudentComponent},
+  { path: 'register', component: AddSudentInfoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+
+}
