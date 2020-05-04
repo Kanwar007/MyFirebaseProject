650d46ea2f75fde95ffad346802d9d97fc3167fd
Kanwar007 <vijayblp.kanwar@gmail.com>
2020-05-04 11:22:45 +0800

    --- Auto-Git Commit ---

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
+
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule }         from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { environment } from 'src/environments/environment';
 import { ListOfStudentComponent } from './list-of-student/list-of-student.component';
 import { AddSudentInfoComponent } from './add-sudent-info/add-sudent-info.component';
 import { EditStudentComponent } from './edit-student/edit-student.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -17,6 +19,7 @@ import { EditStudentComponent } from './edit-student/edit-student.component';
     ListOfStudentComponent,
     AddSudentInfoComponent,
     EditStudentComponent,
+    LoginComponent,
   
   ],
   imports: [
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,54 @@
+
+import { Injectable, NgZone } from '@angular/core';
+import { User } from './../shared/user';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  userData:any;
+
+
+  constructor(
+    public afs:AngularFirestore,
+    public afAuth: AngularFireAuth,
+    public router:Router,
+    public ngZone:NgZone
+
+
+  ) { 
+
+
+ // Sign in with email/password
+ SignIn(email, password) {
+  
+  return  this.afAuth.signInWithEmailAndPassword(email, password)
+    .then((result) => {
+      this.ngZone.run(() => {
+        this.router.navigate(['dashboard']);
+      });
+      
+    }).catch((error) => {
+      window.alert(error.message)
+    })
+}
+
+
+  }
+//login with email/password
+
+  login(email,password)
+  {
+  return this.afAuth.signInWithEmailAndPassword(email,password)
+  .then((result)=> {
+    this.ngZone.run(()=>{
+      this.router.navigate(['list']);
+    });
+
+  }).catch((error)=>{
+    window.alert(error.message)
+  })
+  }
+}
